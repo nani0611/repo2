aab1d4b7ff67a32d6552efd857d59cc70f4c2313
nani0611 <119838859+nani0611@users.noreply.github.com>
2022-12-14 11:08:02 +0530

    Update jenkin.py

--- jenkin.py
+++ jenkin.py
@@ -1,7 +1,1 @@
-list1=[10,20,30,40,50]
-res_list1=[x*2 for in list1]
-print(res_list1)
-list2=[100,200,300,400,500]
-res_list2=[x*0.2 for in list2]
-print(res_list2)
-print('list completed')
+print("hello world")
